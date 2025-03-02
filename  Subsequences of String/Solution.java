aebc807b77b37a1fa7cf4d8fe8b397aaccbbde43
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-03-02 23:29:39 +0530

    Add comments to explain subsequence generation logic

---  Subsequences of String/Solution.java
+++  Subsequences of String/Solution.java
@@ -1,27 +1,31 @@
-import java.util.* ;
+import java.util.*;  // Importing utility package for ArrayList
 import java.io.*; 
 import java.util.ArrayList;
 
 class Solution {
+    // Function to generate all subsequences of a given string
     public static ArrayList<String> subsequences(String str) {
-        // Write your code here
-        ArrayList<String>ans=new ArrayList<>();
-        solve(str,new StringBuilder(),ans,0);
+        ArrayList<String> ans = new ArrayList<>();  // List to store subsequences
+        solve(str, new StringBuilder(), ans, 0);  // Helper function to generate subsequences
         return ans;
     }
-    public static void  solve(String s,StringBuilder sb,ArrayList<String> ans,int i){
 
-        if(i==s.length()){
-            if(sb.length()>0){
+    // Recursive function to generate subsequences
+    public static void solve(String s, StringBuilder sb, ArrayList<String> ans, int i) {
+        // Base case: if we have processed all characters
+        if (i == s.length()) {
+            if (sb.length() > 0) {  // If subsequence is not empty, add it to the list
                 ans.add(sb.toString());
             }
             return;
         }
+
+        // Include current character in subsequence
         sb.append(s.charAt(i));
-        solve(s,sb,ans,i+1);
-        sb.deleteCharAt(sb.length() - 1);
-        solve(s,sb,ans,i+1);
-
-
+        solve(s, sb, ans, i + 1);
+        
+        // Backtrack: remove last character and move to the next possibility
+        sb.deleteCharAt(sb.length() - 1);   
+        solve(s, sb, ans, i + 1); 
     }
 }
