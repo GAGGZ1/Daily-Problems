9cd91f8e6b9ede4927893f6cba47065cd5a05b83
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2025-03-15 21:48:20 +0530

    Solved Problems

--- Search a 2D Matrix/Solution.java
+++ Search a 2D Matrix/Solution.java
@@ -1,23 +1,37 @@
 class Solution {
-  public boolean searchMatrix(int[][] mat, int target) {
-      // convert into flattened 1d array
-      int n = mat.length;
-      int m = mat[0].length;
-      int low = 0;
-      int high = m * n - 1;
-      while (low <= high) {
-          int mid = (low + high) / 2;
-          int row = mid / m;
-          int col = mid % m;
-          if (mat[row][col] == target) {
-              return true;
-              
-          } else if (mat[row][col] < target) {
-              low = mid + 1;
-          } else {
-              high = mid - 1;
-          }
-      }
-      return false;
+    public boolean searchMatrix(int[][] mat, int target) {
+        // Get the number of rows (n) and columns (m)
+        int n = mat.length;
+        int m = mat[0].length;
+  
+        // Define search space in a virtual 1D representation of the 2D matrix
+        int low = 0;
+        int high = m * n - 1;
+  
+        // Perform binary search
+        while (low <= high) {
+            int mid = (low + high) / 2;
+  
+            // Convert mid index into 2D matrix indices
+            int row = mid / m;
+            int col = mid % m;
+  
+            // Check if the target is found
+            if (mat[row][col] == target) {
+                return true;
+            } 
+            // If target is greater, search in the right half
+            else if (mat[row][col] < target) {
+                low = mid + 1;
+            } 
+            // If target is smaller, search in the left half
+            else {
+                high = mid - 1;
+            }
+        }
+  
+        // Target not found
+        return false;
+    }
   }
-}
+  
