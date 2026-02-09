4c04e3a2387c305c70da3591c86e69ac02c0a70b
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-02-09 22:07:55 +0530

    Solved Two pointer + Sliding Window Problem

--- feb-26/3. Longest Substring Without Repeating Characters/Solution.java
+++ feb-26/3. Longest Substring Without Repeating Characters/Solution.java
@@ -18,3 +18,37 @@ class Solution {
         return max;
     }
 }
+
+// more optimised by, moving directly 
+// Instead of removing characters step-by-step:
+
+// left++
+
+
+// We directly move:
+
+// left = lastIndex + 1
+
+
+// No repeated deletions.
+// Fewer operations.
+// Cleaner invariant.
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int max = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int right = 0; right < s.length(); right++) {
+
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)) + 1);
+
+            }
+
+            map.put(s.charAt(right), right);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
