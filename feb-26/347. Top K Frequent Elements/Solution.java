9f0814e5f4d013a09452e564c230b799f39fb1d8
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-02-14 10:15:27 +0530

    Solved Todays Target Problems

---
+++ feb-26/347. Top K Frequent Elements/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
+
+        for(int i:map.keySet()){
+            pq.offer(i);
+            
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int[]res=new int[k];
+        int i=0;
+        while(!pq.isEmpty()){
+            res[i++]=pq.poll();
+        }
+        return res;
+
+
+    }
+}
