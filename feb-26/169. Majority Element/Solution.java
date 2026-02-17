f5c56c0e2e82f4a2e9288b2d46c62027ddea77bf
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-02-17 22:07:21 +0530

    Solved Todays Target Problems

---
+++ feb-26/169. Majority Element/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cand=0;
+        int count=0;
+        for(int n:nums){
+            if(count==0){
+                cand=n;
+            }
+            if(n==cand){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return cand;
+    }
+}
