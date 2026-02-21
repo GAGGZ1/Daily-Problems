bca5bb155993fd2df17828ff160d242d47f158c3
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-02-21 12:44:04 +0530

    Solved Todays Target Problems

--- feb-26/Reverse string using recursion/Solution.java
+++ feb-26/Reverse string using recursion/Solution.java
@@ -3,7 +3,7 @@ public class Solution {
   public static String reverse(String s){
     if(s.length()<=1)return s;
 
-    return s.substring(1)+s.charAt(0);
+    return reverse(s.substring(1))+s.charAt(0);
   }
   public static void main(String[] args){
     System.out.println(reverse("hello"));
