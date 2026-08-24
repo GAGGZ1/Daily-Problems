e9bb718cd71c751a37b8d3e45e478a39694d40f7
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2026-08-24 23:49:58 +0530

    Design an algorithm for computing the black region that contains the most points.

---
+++ Graph/find max black color region/Solution.java
@@ -0,0 +1,38 @@
+
+
+public class Solution {
+  static int dir[][]={{-1,0},{1,0},{0,1},{0,-1}};
+  public static void main(String args[]){
+    char[][]graph={{'w','w','b','b','w'},{'w','b','b','w','w'},{'w','w','w','w','b'},{'b','b','w','b','b'},{'b','w','w','b','b'},};
+    int max=0;
+    int n=graph.length;
+    int m=graph[0].length;
+    boolean[][]visi=new boolean[n][m];
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        if(graph[i][j]=='b' && visi[i][j]!=true){
+          max=Math.max(max,findRegions(graph, i, j, n, m,  visi));
+        }
+      }
+    }
+    System.out.println("max black region = "+max);
+
+  }
+  public static int findRegions(char[][] graph,int i,int j,int n,int m,boolean visi[][]){
+    if(i>=n || j>=m || i<0 || j<0 || graph[i][j]=='w' || visi[i][j]==true){
+      return 0;
+    }
+    visi[i][j]=true;
+    int count=1;
+    
+    for(int k=0;k<4;k++){
+      int ni=i+dir[k][0];
+      int nj=j+dir[k][1];
+      
+      count+=findRegions(graph, ni, nj, n, m, visi);
+    }
+    return count;
+
+
+  }
+}
